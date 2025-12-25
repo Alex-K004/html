9bb7356a4c4dbcf7f0d62d39e8c596ab14034030
Neto test <akochetkov1312@gmail.com>
2025-12-25 20:30:27 +0300

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: isProduction ? '/popover-widget/' : '/',
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
       },
+      compress: true,
+      port: 9000,
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.[contenthash].css'
+      })
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    static: './dist',
-    open: true,
-    port: 9004,
-  },
-  mode: 'development',
+    devtool: isProduction ? false : 'source-map'
+  };
 };
