cfa12f352e97357003b996fc8ebea62a3e1dfb8d
Neto test <akochetkov1312@gmail.com>
2025-12-26 18:30:22 +0300

    Two commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  
-  return {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.[contenthash].js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-    },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
-      compress: true,
-      port: 9009,
-      open: true,
-      hot: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader'
-          ]
-        },
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'body'
-      }),
-      new MiniCssExtractPlugin({
-        filename: 'styles.[contenthash].css'
-      })
-    ],
-    devtool: isProduction ? false : 'source-map'
-  };
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
 };
