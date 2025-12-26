46d13667dba9abef7fb4404b05fccab303226273
Neto test <akochetkov1312@gmail.com>
2025-12-26 18:25:53 +0300

    Two commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
     output: {
       filename: 'bundle.[contenthash].js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: isProduction ? '/popover-widget/' : '/',
+      publicPath: '/',
     },
     devServer: {
       static: {
